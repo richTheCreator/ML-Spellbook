90eb9608c2f63863e8505102c782493df449fbae
richTheCreator <moralesrichard@live.com>
2018-09-14 08:59:44 -0500

    doczrc.js config file added.

---
+++ doczrc.js
@@ -0,0 +1,16 @@
+export default {
+  title: '📖 Spellbook',
+  description: 'The design system that powers Merlin Labs.',
+  // theme: 'my-coolest-theme',
+  // themeConfig: {
+  //   colors: {
+  //     primary: 'tomato'
+  //   }
+  // },
+  // modifyBundlerConfig: config => {
+  //  /* do your magic here */
+  // },
+  // plugins: [
+  //   myCoolPlugin()
+  // ]
+}
